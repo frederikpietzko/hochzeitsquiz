7367fdc9029aeb3be73a1e24923bcbfa7954ab1c
Frederik Pietzko <frederik.pietzko@inform-software.com>
2022-07-23 01:27:45 +0200

    Added Manual reset Button.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import { trpc } from '../utils/trpc';
 
 export const Navigation: React.FC = () => {
+  const { mutate: resetQuestions } = trpc.useMutation('resetQuestions');
   return (
     <div className="text-lg underline flex gap-2">
       <Link href="/questions">Liste</Link>
       <Link href="/questions/create">Erstellen</Link>
+      <button
+        className="bg-red-500 px-2 py-1 rounded "
+        onClick={() => resetQuestions()}
+      >
+        Reset
+      </button>
     </div>
   );
 };
